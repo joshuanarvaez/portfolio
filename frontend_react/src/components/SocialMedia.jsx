9dc6c9d4ff122ffa53bd86efdb70b5f1492ba793
Josh Narvaez <jhnarva@gmail.com>
2022-11-14 20:21:28 -0600

    first commit

---
+++ frontend_react/src/components/SocialMedia.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+      <BsLinkedin />
+    </div>
+    <div>
+      <BsGithub />
+    </div>
+  </div>
+);
+
+export default SocialMedia;
